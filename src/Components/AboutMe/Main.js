08fd55aa90bd05655c1b40bf3e0dfa202129f9fd
Navdeep Boparai <navdeep.boparai@triosstudent.com>
2018-12-14 21:03:59 -0500

    portfolio

--- src/Components/AboutMe/Main.js
+++ src/Components/AboutMe/Main.js
@@ -7,16 +7,9 @@ const Main=()=> {
 
     return (
         <div className="jumbotron text-center">
-            <h1>Company</h1>
-            <p>We specialize in blablabla</p>
-            <form>
-                <div className="input-group">
-                    <input type="email" className="form-control" size="50" placeholder="Email Address" required>
-                        <div className="input-group-btn">
-                            <button type="button" className="btn btn-danger">Subscribe</button>
-                        </div>
-                </div>
-            </form>
+            <h1>Hi</h1>
+            <h3> My name is Navdeep  Boparai</h3>
+
         </div>
     )
 }
--- src/Components/AboutMe/Navigation.js
+++ src/Components/AboutMe/Navigation.js
@@ -1,31 +1,58 @@
 import React  from 'react';
-
+import Image from '../../images/img_avatar3.png';
 
 
 const Navigation=()=>{
 
     return(
-        <nav className="navbar navbar-default navbar-fixed-top">
-            <div className="container">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
 
-                </div>
+<div >
 
-                <div className="collapse navbar-collapse" id="myNavbar">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><a href="#about">ABOUT</a></li>
-                        <li><a href="#services">EDUCATION</a></li>
-                        <li><a href="#portfolio">WORK </a></li>
-                        <li><a href="#contact">CONTACT</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
+                    <nav className="navbar navbar-default navbar-fixed-top">
+                        <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+
+                        </div>
+
+                        <div className="collapse navbar-collapse" id="myNavbar">
+                            <ul className="nav navbar-nav navbar-left">
+                                <li><a href="#about">ABOUT</a></li>
+                                <li><a href="#services">EDUCATION</a></li>
+                                <li><a href="#portfolio">WORK </a></li>
+                                <li><a href="#contact">CONTACT</a></li>
+                            </ul>
+                        </div>
+                        </div>
+                    </nav>
+                      <div className="row">
+                            <div className="col-sm-4">
+                                 <span className="glyphicon glyphicon-signal logo"></span>
+                            </div>
+                    </div>
+
+
+<div className="container-fluid bg-grey">
+        <div className="row">
+                    <div className="col-sm-4">
+                        <span classNmae="glyphicon glyphicon-globe logo slideanim"></span>
+                     </div>
+<div className="col-sm-8">
+        <h2>Our Values</h2>
+    <h4><strong>MISSION:</strong> Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
+    <p><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+</div>
+</div>
+
+</div>
 
 
 
